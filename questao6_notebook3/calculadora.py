514911ba8c980aaa2ec5df02978ae52ae8d2f722
phmcd-bit <phmacedoalves@gmail.com>
2026-07-03 00:54:09 +0000

    Adicionando a questão 6 do notebook 3 do Trilha ao GitHub

---
+++ questao6_notebook3/calculadora.py
@@ -0,0 +1,9 @@
+def somar(a, b):
+    return a + b
+
+def multiplicar(a, b):
+    return a * b
+
+if __name__ == '__main__':
+    print(somar(2, 3))
+    print(multiplicar(2, 3))
---
+++ questao6_notebook3/main.py
@@ -0,0 +1,4 @@
+import calculadora
+
+print(calculadora.somar(2, 3))
+print(calculadora.multiplicar(2, 3))
